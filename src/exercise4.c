8385c0aea918f1e044330cb50fcf793c48864a8c
Балабух Андрей Олегович ИП-516 <andrey@localhost.localdomain>
2025-10-15 16:22:59 +0700

    hbdsq2e1e2fdswfeddwehui

--- src/exercise4.c
+++ src/exercise4.c
@@ -3,20 +3,19 @@
 
 int main(int argc, char** argv) {
     float a[10];
-    int k = atof(argv[11]);
-    float x = atoi(argv[12]);
+    float x = atof(argv[11]);
+    int k = atoi(argv[12]);
 
     for (int i = 0; i <= 9; i++) {
 	scanf("%f", &a[i]);
     }
 
-    for (int i = 0; i <= 9; i++) {
-	if (k == i) {
-	    a[k] = x;
-	    printf("%.2f ", a[k]);
-        } else {
-	    printf("%.2f ", a[i]);
-	}
+    for (int i = 9; i > k; i--) {
+        a[i] = a[i-1];
+    }
+    a[k] = x;
+    for (int i = 0; i < 10; i++){
+	printf("%f", a[i]);
     }
     return 0;
 }

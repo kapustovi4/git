7e513a394e99caee92683b09d25e248c86d306b3
Балабух Андрей Олегович ИП-516 <andrey@localhost.localdomain>
2025-10-15 16:36:05 +0700

    hbdsq2e1e2fdswfeddwehuigihew

--- src/exercise3.c
+++ src/exercise3.c
@@ -6,11 +6,11 @@ int main(int argc, char** argv) {
     int k = atoi(argv[11]);
 
     for (int i = 0; i <= 10; i++) {
-	a[i] = atof(argv[i+1]);
+	scanf("%f", &a[i]);
     }
 
     for (int i = 0; i <= 10; i++) {
-	printf("%f", a[(i+k)%10]);
+	printf("%f", a[(i - k + 10)%10]);
     }
     return 0;
 }

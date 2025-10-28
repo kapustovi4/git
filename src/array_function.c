fd4a4cd25c43848622c72439931ad82ceabc66a7
Андрей Балабух Олегович ИП-516 <andrey@CABBAGE-NOTE.localdomain>
2025-10-28 21:29:00 +0700

    реализована функция join_int_arrays

--- src/array_function.c
+++ src/array_function.c
@@ -1,6 +1,13 @@
 #include "../include/array_function.h"
 
-void join_int_arrays(int *src1, size_t size_src1, int *src2, size_t size_src2, int *dest) {}
+void join_int_arrays(int *src1, size_t size_src1, int *src2, size_t size_src2, int *dest) {
+    for(size_t i = 0; i < size_src1; i++) {
+        dest[i] = src1[i];
+    }
+    for(size_t i = 0; i < size_src2; i++) {
+        dest[size_src1 + i] = src2[i];
+    }
+}
 
 void join_and_sort_int_arrays(int* src1, size_t size_src1, int* src2, size_t size_src2, int* dest) {}
 

10f24d218ea7d63b532e20c2af14b12bb8a21e5f
Андрей Балабух Олегович ИП-516 <andrey@CABBAGE-NOTE.localdomain>
2025-10-28 22:56:45 +0700

    фикс(1) функции get_min_and_max_from_int_array

--- src/array_function.c
+++ src/array_function.c
@@ -33,11 +33,11 @@ void get_min_and_max_from_int_array(int* src, size_t size, int* min, int* max) {
     *max = src[0];
 
     for (int i = 0; i < size; i++) {
-        if (src[i] < min) {
-            min = src[i]; 
+        if (src[i] < *min) {
+            *min = src[i]; 
         }
-        if (src[i] > max) {
-            max = src[i];
+        if (src[i] > *max) {
+            *max = src[i];
         }
     }
 }

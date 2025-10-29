4d05eb7d0318b029f000072467caac69176d707d
Андрей Балабух Олегович ИП-516 <andrey@CABBAGE-NOTE.localdomain>
2025-10-29 16:51:11 +0700

    lol

--- src/array_function.c
+++ src/array_function.c
@@ -14,30 +14,38 @@ void join_and_sort_int_arrays(int* src1, size_t size_src1, int* src2, size_t siz
 
     while (i < size_src1 && j < size_src2) {
         if (src1[i] <= src2[j]) {
-            dest[k++] = src1[i++];
+            dest[k] = src1[i];
+            k = k++;
+            i = i++;
         } else {
-            dest[k++] = src2[j++];
+            dest[k] = src2[j];
+            k = k++;
+            j = j++;
         }
     }
     while (i < size_src1) {
-        dest[k++] = src1[i++];
+        dest[k] = src1[i];
+        k = k++;
+        i = i++;
     } 
     while (j < size_src2) {
-        dest[k++] = src2[j++];
+        dest[k] = src2[j];
+        k = k++;
+        j = j++;
     }
 
 }
 
 void get_min_and_max_from_int_array(int* src, size_t size, int* min, int* max) {
-    *min = src[0];
-    *max = src[0];
+    min = src[0];
+    max = src[0];
 
     for (int i = 0; i < size; i++) {
-        if (src[i] < *min) {
-            *min = src[i]; 
+        if (src[i] < min) {
+            min = src[i]; 
         }
-        if (src[i] > *max) {
-            *max = src[i];
+        if (src[i] > max) {
+            max = src[i];
         }
     }
 }
